6bbf5478fa67a510f562e0df1f57a596a26828c7
Henry Till <henrytill@gmail.com>
2017-03-26 18:19:06 -0400

    update laziness.ml

--- laziness.ml
+++ laziness.ml
@@ -1,16 +1,42 @@
-(* https://existentialtype.wordpress.com/2011/04/24/the-real-point-of-laziness/ *)
+(** Lazy datatypes inspired by
+    {{: https://existentialtype.wordpress.com/2011/04/24/the-real-point-of-laziness/ }
+    {i The Point of Laziness}}, by Bob Harper
+  *)
 
 module type PROCESS = sig
+  (** Represents a function that, when applied, generates a value of some type *)
   type 'a t = unit -> 'a option
-  (** Represents a function that, when applied, generates a value of some
-      type *)
 
-  val stdin : char t
   (** Represents the UNIX standard input *)
+  val stdin : char t
 
-  val random : int t
   (** Represents a random number generator *)
+  val random : int t
 end
+
+
+module type STREAM = sig
+  type 'a t
+
+  (** An ephemeral process of creation *)
+  type 'a process
+
+  (** The type of values that arise when the stream is exposed *)
+  type 'a front =
+    | Nil
+    | Cons of 'a * 'a t
+
+  (** Exposes the stream *)
+  val expose : 'a t -> 'a front
+
+  (** Creates a persistent stream from an ephemeral process of creation for its elements *)
+  val memo : 'a process -> 'a t
+
+  (* Creates recursive networks of streams *)
+  (* val fix : ('a t -> 'a t) -> 'a t *)
+end
+
+module type STREAM_FUNCTOR = functor (P : PROCESS) -> STREAM with type 'a process := 'a P.t
 
 module Process : PROCESS = struct
   type 'a t = unit -> 'a option
@@ -18,26 +44,8 @@ module Process : PROCESS = struct
   let random () =  Some (Random.int 127)
 end
 
-module type STREAM_FUNCTOR = functor (P : PROCESS) -> sig
-  type 'a t
+module MakeStream : STREAM_FUNCTOR = functor (P : PROCESS) -> struct
 
-  type 'a front =
-    | Nil
-    | Cons of 'a * 'a t
-    (** The type of values that arise when the stream is exposed *)
-
-  val expose : 'a t -> 'a front
-  (** Exposes the stream *)
-
-  val memo : 'a P.t -> 'a t
-  (** Creates a persistent stream from an ephemeral process of creation for its
-      elements *)
-
-  (* val fix : ('a t -> 'a t) -> 'a t *)
-  (** Creates recursive networks of streams *)
-end
-
-module Stream : STREAM_FUNCTOR = functor (P : PROCESS) -> struct
   type 'a t = Stream of (unit -> 'a front)
 
   and 'a front =
@@ -52,7 +60,7 @@ module Stream : STREAM_FUNCTOR = functor (P : PROCESS) -> struct
     | None   -> Stream (fun () -> Nil)
 end
 
-module ProcessStream = Stream (Process)
+module ProcessStream = MakeStream(Process)
 
 exception End
 
