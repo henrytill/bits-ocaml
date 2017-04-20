32cbfc6a83952da8379ca7bc3956bd14b7b2b1f7
Henry Till <henrytill@gmail.com>
2017-04-20 13:41:13 -0400

    update landins_knot.ml

--- landins_knot.ml
+++ landins_knot.ml
@@ -6,10 +6,10 @@ let landins_knot f =
   fixedpoint
 
 let factorial =
-  let g f x =
+  let f g x =
     if x = 0 then
       1
     else
-      x * f (x - 1)
+      x * g (x - 1)
   in
-  landins_knot g
+  landins_knot f
