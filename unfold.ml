af0e9cfc7219d8daf77ba49a97bd8ff161e6d28f
Henry Till <henrytill@gmail.com>
2017-04-20 19:39:08 -0400

    update unfold.ml

--- unfold.ml
+++ unfold.ml
@@ -1,10 +1,8 @@
-(* val unfoldr : ('a -> ('b * 'a) option) -> 'a -> 'b list *)
 let rec unfoldr f seed =
   match f seed with
   | None            -> []
   | Some (v, seed') -> v :: (unfoldr f seed')
 
-(* val schemeunfold : ('a -> bool) -> ('a -> 'b) -> ('a -> 'a) -> ('a -> 'b list) *)
 let schemeunfold isempty head tail seed =
   unfoldr
     (fun seed -> if isempty seed then None else Some(head seed, tail seed))
