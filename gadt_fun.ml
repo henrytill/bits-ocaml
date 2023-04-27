819618b6a61a35bdce321a38331f9642ca48a6fe
Henry Till <henrytill@gmail.com>
2023-04-26 18:00:17 -0700

    add gadt_fun.ml

---
+++ gadt_fun.ml
@@ -0,0 +1,28 @@
+(* natural numbers  *)
+type zero = Zero
+type 'a succ = Succ of 'a
+
+(* length-indexed vector *)
+type (_,_) vec =
+  | Nil  : ('a, zero) vec
+  | Cons : 'a * ('a, 'n) vec -> ('a, 'n succ) vec
+
+(* map over a length-indexed vector, preserving length *)
+let rec map : type a b n. (a -> b) -> (a, n) vec -> (b, n) vec =
+  fun f -> function
+    | Nil         -> Nil
+    | Cons (a, l) -> Cons (f a, map f l)
+;;
+
+(* head is exhaustive on vectors of length at least 1 *)
+let head : type a n. (a, n succ) vec -> a =
+  function Cons (a, _) -> a
+;;
+
+(* equality witness *)
+type (_,_) eq = Refl : ('a, 'a) eq
+
+(* type-safe cast *)
+let cast : type a b. (a, b) eq -> a -> b =
+  function Refl -> fun x -> x
+;;
