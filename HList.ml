57bbdd8fd019486319f66c2b5884ffe6f122e9c1
Henry Till <henrytill@gmail.com>
2023-04-26 21:15:39 -0700

    HList.ml: tweak

--- HList.ml
+++ HList.ml
@@ -7,21 +7,25 @@ type _ t =
   | HCons : 'a * 'b t -> ('a * 'b) t
 
 type (_, _) index =
-  | Z : ('x * 'xs, 'x) index
-  | S : ('xs, 'x) index -> ('y * 'xs, 'x) index
+  | Z : ('a * 'xs, 'a) index
+  | S : ('xs, 'a) index -> ('x * 'xs, 'a) index
 
-let rec get : type tys ty. (tys, ty) index -> tys t -> ty =
+let rec get : type a b. (a, b) index -> a t -> b =
   fun index l -> match index, l with
     | Z  , HCons (h, _) -> h
     | S e, HCons (_, t) -> get e t
     | _  , HNil         -> .
+;;
 
 let rec length : type tys. tys t -> int = function
   | HNil         -> 0
   | HCons (_, t) -> 1 + length t
+;;
 
 let hhead = function
   | HCons (h, _) -> h
+;;
 
 let htail = function
   | HCons (_, t) -> t
+;;
