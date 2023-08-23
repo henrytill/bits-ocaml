fe8ff6503b085cde58b56640c686f760bff85474
Henry Till <henrytill@gmail.com>
2023-08-22 23:37:36 -0700

    landins_knot.ml: add type signatures

--- landins_knot.ml
+++ landins_knot.ml
@@ -1,11 +1,11 @@
 (** "Landin's Knot" - implements recursion by backpatching *)
-let landins_knot f =
+let landins_knot : (('a -> 'b) -> 'a -> 'b) -> 'a -> 'b = fun f ->
   let r = ref (fun x -> assert false) in
   let fixedpoint = f (fun x -> !r x) in
   r := fixedpoint;
   fixedpoint
 
-let factorial =
+let factorial : int -> int =
   let f g x =
     if x = 0 then
       1
