36b7917434eaaa080c3b26864639a2cc8d6dc442
Henry Till <henrytill@gmail.com>
2023-08-24 04:13:51 -0700

    build-static-exe.sh: tweak

--- build-static-exe.sh
+++ build-static-exe.sh
@@ -7,11 +7,11 @@ TAG=${TAG:-ocaml-bits:latest}
 DOCKER_BUILD_DIR=${DOCKER_BUILD_DIR:-/home/build/ocaml-bits}
 
 if [ -z "$FILES" ]; then
-    echo "Usage: ${0} <file1> <file2> ..."
+    echo "Usage: sh ${0} <file1> <file2> ..."
     exit 1
 fi
 
-build () {
+build() {
     docker build --tag "$1" --progress=plain "$(pwd)"
 }
 
