c9351b4071e76c512fab9f54a6ed3566577c41dd
Henry Till <henrytill@gmail.com>
2024-02-21 23:39:41 -0800

    lenses: tweak

--- lenses.ml
+++ lenses.ml
@@ -6,7 +6,7 @@ class type ['s, 't, 'a, 'b] lens = object
   method set : 'b -> 's -> 't
 end
 
-type ('s, 'a) lens_prime = ('s, 's, 'a, 'a) lens
+type ('s, 'a) lens' = ('s, 's, 'a, 'a) lens
 
 let compose this that =
   object
@@ -40,26 +40,29 @@ let fred =
 
 (* Some lenses for the data *)
 
-let name : (person, string) lens_prime =
+let name : (person, string) lens' =
   object
     method get x = x._name
     method set v x = { x with _name = v }
   end
 
-let addr : (person, address) lens_prime =
+let addr : (person, address) lens' =
   object
     method get x = x._addr
     method set v x = { x with _addr = v }
   end
 
-let postcode : (address, string) lens_prime =
+let postcode : (address, string) lens' =
   object
     method get x = x._postcode
     method set v x = { x with _postcode = v }
   end
 
-(* Some examples *)
-
-let set_postcode = set (compose addr postcode)
-let test1 = get fred (compose addr postcode)
-let new_fred = set_postcode "555555" fred
+(* Example *)
+let () =
+  let fred_postcode = get fred (compose addr postcode) in
+  let set_postcode = set (compose addr postcode) in
+  print_endline ("Fred's postcode: " ^ fred_postcode);
+  let new_fred = set_postcode "55555" fred in
+  let fred_postcode = get new_fred (compose addr postcode) in
+  print_endline ("Fred's updated postcode: " ^ fred_postcode)
