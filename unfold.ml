6ef0a49efb30978cd2d91ccb5ead940aaf35464d
Henry Till <henrytill@gmail.com>
2024-03-03 14:24:58 -0800

    unfold: add return type

--- unfold.ml
+++ unfold.ml
@@ -3,7 +3,7 @@ let[@tail_mod_cons] rec unfoldr f seed =
   | None -> []
   | Some (v, seed') -> v :: unfoldr f seed'
 
-let schemeunfold (isempty : 'a -> bool) (head : 'a -> 'b) (tail : 'a -> 'a) (seed : 'a) =
+let schemeunfold (isempty : 'a -> bool) (head : 'a -> 'b) (tail : 'a -> 'a) (seed : 'a) : 'b list =
   unfoldr
     (fun seed ->
       if isempty seed then
