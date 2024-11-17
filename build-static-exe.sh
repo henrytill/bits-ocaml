e2b4a039d078594bd51c7a7ec2d99607ed78e965
Henry Till <henrytill@gmail.com>
2024-11-16 16:09:59 -0800

    build-static-exe.sh: reformat

--- build-static-exe.sh
+++ build-static-exe.sh
@@ -7,25 +7,25 @@ TAG=${TAG:-bits-ocaml:latest}
 DOCKER_BUILD_DIR=${DOCKER_BUILD_DIR:-/home/build/bits-ocaml}
 
 if [ -z "$FILES" ]; then
-  echo "Usage: sh ${0} <file1> <file2> ..." >&2
-  exit 1
+    echo "Usage: sh ${0} <file1> <file2> ..." >&2
+    exit 1
 fi
 
 build() {
-  docker build --tag "$1" --progress=plain "$(pwd)"
+    docker build --tag "$1" --progress=plain "$(pwd)"
 }
 
 create() {
-  docker create "$1"
+    docker create "$1"
 }
 
 copy() {
-  echo "Copying ${2}"
-  docker cp -q "${1}:${DOCKER_BUILD_DIR}/${2}" "$(pwd)/_static/${2}"
+    echo "Copying ${2}"
+    docker cp -q "${1}:${DOCKER_BUILD_DIR}/${2}" "$(pwd)/_static/${2}"
 }
 
 remove() {
-  docker remove "$1" >/dev/null
+    docker remove "$1" >/dev/null
 }
 
 build "$TAG"
@@ -36,7 +36,7 @@ mkdir -p _static/src
 mkdir -p _static/test
 
 for file in $FILES; do
-  copy "$cid" "$file"
+    copy "$cid" "$file"
 done
 
 remove "$cid"
