a0231d30685315ecef7a618f639e56c28b83ec80
Henry Till <henrytill@gmail.com>
2025-02-02 14:56:58 -0800

    revamp build

--- flake.nix
+++ flake.nix
@@ -1,17 +1,20 @@
 {
   inputs = {
-    opam-nix.url = "github:tweag/opam-nix";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+    opam-nix = {
+      url = "github:tweag/opam-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.opam-repository.follows = "opam-repository";
+    };
     flake-utils = {
       url = "github:numtide/flake-utils";
       follows = "opam-nix/flake-utils";
     };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
-
   outputs =
     {
       self,
@@ -20,59 +23,24 @@
       nixpkgs,
       ...
     }@inputs:
-    # Don't forget to put the package name instead of `throw':
     let
-      package = "bits-ocaml";
+      package = "bits";
     in
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         on = opam-nix.lib.${system};
-        devPackagesQuery = {
-          # You can add "development" packages here. They will get added to the devShell automatically.
-          merlin = "*";
-          ocp-indent = "*";
+        scope = on.buildOpamProject { resolveArgs.with-test = true; } package ./. {
+          ocaml-base-compiler = "5.2.0";
         };
-        query = devPackagesQuery // {
-          ## You can force versions of certain packages here, e.g:
-          ## - force the ocaml compiler to be taken from opam-repository:
-          ocaml-base-compiler = "4.14.1";
-          ## - or force the compiler to be taken from nixpkgs and be a certain version:
-          # ocaml-system = "4.14.0";
-          ## - or force ocamlfind to be a certain version:
-          # ocamlfind = "1.9.2";
-        };
-        scope = on.buildOpamProject' { } ./. query;
         overlay = final: prev: {
-          # You can add overrides here
-          ${package} = prev.${package}.overrideAttrs (_: {
-            # Prevent the ocaml dependencies from leaking into dependent environments
-            doNixSupport = false;
-          });
-          delimcc = prev.delimcc.overrideAttrs (oa: {
-            postPatch = ''
-              sed -i 's|\$(OCAMLBIN)/||g' Makefile
-            '';
-          });
+          ${package} = prev.${package}.overrideAttrs (as: { });
         };
-        scope' = scope.overrideScope' overlay;
-        # The main package containing the executable
-        main = scope'.${package};
-        # Packages from devPackagesQuery
-        devPackages = builtins.attrValues (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
       in
       {
-        legacyPackages = scope';
-
-        packages.default = main;
-
-        devShells.default = pkgs.mkShell {
-          inputsFrom = [ main ];
-          buildInputs = devPackages ++ [
-            # You can add packages from nixpkgs here
-          ];
-        };
+        legacyPackages = scope.overrideScope overlay;
+        packages.default = self.legacyPackages.${system}.${package};
       }
     );
 }
