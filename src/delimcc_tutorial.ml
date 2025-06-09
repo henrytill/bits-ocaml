986d9c6c9e18c6dac94da14b4e92b89dc8c37f85
Henry Till <henrytill@gmail.com>
2025-06-09 16:57:38 -0700

    Delimcc_tutorial: add type annotations

--- src/delimcc_tutorial.ml
+++ src/delimcc_tutorial.ml
@@ -9,10 +9,10 @@ let make_ops () =
 (* Section 2.1 *)
 module Exercise_01 = struct
   (* [2 * 3] : int *)
-  let no_01 = 5 * ((2 * 3) + (3 * 4))
+  let no_01 = 5 * ((2 * 3 : int) + (3 * 4))
 
   (* [2 = 3] : bool *)
-  let no_02 = (if 2 = 3 then "hello" else "hi") ^ " world"
+  let no_02 = (if (2 = 3 : bool) then "hello" else "hi") ^ " world"
 
   (* [let x = 1] : int *)
   let no_03 =
@@ -21,7 +21,7 @@ module Exercise_01 = struct
        (x, x))
 
   (* [3 + 1] : int *)
-  let no_04 = String.length ("x" ^ string_of_int (3 + 1))
+  let no_04 = String.length ("x" ^ string_of_int (3 + 1 : int))
 end
 
 module Exercise_05 = struct
