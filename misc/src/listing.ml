6d01f7f28e48e5ad72d9056e1fa7751b132fea4c
Henry Till <henrytill@gmail.com>
2025-08-19 00:26:07 -0700

    Listing: tweak

--- misc/src/listing.ml
+++ misc/src/listing.ml
@@ -12,29 +12,29 @@ module List_monad = struct
   let bind m f = List.flatten (List.map f m)
 end
 
-module type Applicative_syntax = sig
+module type APPLICATIVE_SYNTAX = sig
   type 'a t
 
   val ( let+ ) : 'a t -> ('a -> 'b) -> 'b t
   val ( and+ ) : 'a t -> 'b t -> ('a * 'b) t
 end
 
-module type Monad_syntax = sig
-  include Applicative_syntax
+module type MONAD_SYNTAX = sig
+  include APPLICATIVE_SYNTAX
 
   val ( let* ) : 'a t -> ('a -> 'b t) -> 'b t
   val ( and* ) : 'a t -> 'b t -> ('a * 'b) t
+  val return : 'a -> 'a t
 end
 
-module Make_syntax (M : MONAD) : Monad_syntax with type 'a t = 'a M.t = struct
+module Make_syntax (M : MONAD) : MONAD_SYNTAX with type 'a t = 'a M.t = struct
   type 'a t = 'a M.t
 
   let ( let+ ) m f = M.bind m (fun a -> M.return (f a))
   let ( and+ ) ma mb = M.bind ma (fun a -> M.bind mb (fun b -> M.return (a, b)))
   let ( let* ) = M.bind
   let ( and* ) = ( and+ )
+  let return = M.return
 end
 
 include Make_syntax (List_monad)
-
-let return = List_monad.return
